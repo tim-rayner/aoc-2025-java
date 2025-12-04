bea1eb0b4ce5aba9e578694a48b83b8c6137dd0c
tim-rayner <tim.rayner2020@gmail.com>
2025-12-04 00:03:08 +0000

    feat(d3c1): implement day 3 challege 1

--- src/main/java/com/timrayner/aoc2025/day03/Day03.java
+++ src/main/java/com/timrayner/aoc2025/day03/Day03.java
@@ -1,22 +1,123 @@
 package com.timrayner.aoc2025.day03;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 import com.timrayner.aoc2025.DayTemplate;
 
 public class Day03 extends DayTemplate {
 
-    private static final String INPUT = "day03.txt";
+    /**
+     * Learning the hard way 
+     * Today I got so carried away trying to solve problem 1 in a extendible way that i ended up confusing myself and not even fully grasping task 1. this led to hours lost and lots of 
+     * frustration... annoying, but its my first AoC and i'm aware some mistakes will be made first time around. Instead of solving part 2 with ai; i've left it as i'm currently flagging at 23:59. 
+     */
 
     @Override
     public String challenge1() throws Exception {
-        var lines = readInput(INPUT);
-        return "NOT IMPLEMENTED 🎅🏼";
+
+        ArrayList<Integer> largestNums = new ArrayList<>();
+
+        try {
+
+            File input = new File("src/main/resources/inputs/day03.txt");
+            try (Scanner johnnyFive = new Scanner(input)) {  // nobody scans chunks of text like J5 🤖
+
+                while(johnnyFive.hasNextLine()){
+
+                    String line = johnnyFive.nextLine().trim();
+                    List<Integer> bank = getBank(line);
+
+                    int largestNum = getLargest(bank);
+
+                    int largestIndex = bank.indexOf(largestNum);
+
+                    List<Integer> newArr;
+
+
+                if(largestIndex >= bank.size() - 1){
+                    //split list before index
+                    newArr = bank.subList(0, bank.size() - 1);
+                    
+                }
+                else {
+                    //split list after index
+                    newArr = bank.subList(largestIndex + 1, bank.size());
+                }
+       
+                //get largest number in array
+
+                int secondLargest = getLargest(newArr);
+                String resultNumber; 
+
+                if(largestNum == bank.get(bank.size() - 1)) {
+                    resultNumber = Integer.toString(secondLargest) + Integer.toString(largestNum);
+                }
+                else {
+                    resultNumber = Integer.toString(largestNum) + Integer.toString(secondLargest);
+                }
+                //add number to total
+                largestNums.add(Integer.parseInt(resultNumber));
+
+                }
+            }
+            
+            int finalNumber = 0;
+
+            for(int i = 0; i < largestNums.size(); i++){
+                System.out.println(largestNums.get(i));
+                finalNumber += largestNums.get(i);
+            }
+
+            return Integer.toString(finalNumber);
+        }
+        catch (FileNotFoundException e) {
+            System.out.println("Error! File not found!");
+        }
+
+        
+        return "A Reindeer seems to be poorly :(";
     }
 
     @Override
     public String challenge2() throws Exception {
-        var lines = readInput(INPUT);
         return "NOT IMPLEMENTED 🎅🏼";
     }
+
+    private static List<Integer> getBank(String input){
+        // convert string to integer array
+        List<Integer> bank = new ArrayList<>();
+
+        for (int i = 0; i < input.length(); i++) {
+            bank.add((input.charAt(i) - '0'));
+        }
+        
+        return bank;
+    }
+
+
+    private static int getLargest(List<Integer> bank){
+
+        int max = bank.get(0); 
+
+        for(int i = 0; i < bank.size(); i++){
+
+            if(bank.get(i) > max) {
+                max = bank.get(i);
+            }
+            
+        }
+
+        return max; 
+
+    } 
+        
+
+
+
 
     
 }
--- src/test/java/com/timrayner/aoc2025/day03/Day03Test.java
+++ src/test/java/com/timrayner/aoc2025/day03/Day03Test.java
@@ -1,20 +1,15 @@
 package com.timrayner.aoc2025.day03;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import org.junit.jupiter.api.Test;
 
 class Day03Test {
 
     @Test
-    void testChallenge1() throws Exception {
+    void testSolution() throws Exception {
         var day = new Day03();
-        assertNotNull(day.challenge1());
-    }
-
-    @Test
-    void testChallenge2() throws Exception {
-        var day = new Day03();
-        assertNotNull(day.challenge2());
+        assertNotNull(day.solution());
     }
     
 }
