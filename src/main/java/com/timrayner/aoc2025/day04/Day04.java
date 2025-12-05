1650291dc0b85a64b043da6bd8783d9c6ae8272f
tim-rayner <tim.rayner2020@gmail.com>
2025-12-05 00:16:25 +0000

    chore(d4): justified day 4 miss

--- src/main/java/com/timrayner/aoc2025/day04/Day04.java
+++ src/main/java/com/timrayner/aoc2025/day04/Day04.java
@@ -2,6 +2,13 @@ package com.timrayner.aoc2025.day04;
 
 import com.timrayner.aoc2025.DayTemplate;
 
+/**
+ * DAY MISSED - NOT ATTEMPTED: 
+ * A combination of deadlines at work, along with personal commitments 
+ * meant we didn't have time to attempt, or solve day 4. 
+ * 
+ * Straight on the naughty list  🙅🏼‍♂️☹️
+ */
 public class Day04 extends DayTemplate {
 
     private static final String INPUT = "day04.txt";
